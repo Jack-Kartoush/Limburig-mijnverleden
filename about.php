22eb4308fef28776b19973f102dc8120195239d8
BestUserNameYouWillSee <77840705+BestUserNameYouWillSee@users.noreply.github.com>
2022-01-27 14:12:11 +0100

    about page

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About the website</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mukta:wght@300&family=Noto+Serif&family=Source+Serif+4:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="about_container">
+        <div class="about_inhoud">
+            Bedankt voor het bezoeken van onze website. Het is een prototype gemaakt voor Kindante. Deze opdracht kregen we van school als project voor een projectweek. Dit opdracht moesten we met een groep van 3 tot 5 mensen doen. De opdracht was om een applicatie
+            te maken waarbij we de locale historie aanraakbaar maakten. Het moest een interactieve zoektocht zijn die kinderen klassikaal of met zijn tweeen kunnen doen. We konden uit 3 thema's kiezen. Deze thema's zijn: Bokkenrijders, Romeinen en het Limburg 
+            mijnverleden. Onze project leider had een link website/app gegeven die we konden gebruiken als inspiratie. Deze app heet Actionbound.
+            <br/><br/>
+            (dit is gewoon een uitleg van waarvoor de (web) applicatie gemaakt is en waarom)
+        </div>
+        <div class="about_footer">
+            <?php include("./includs/footer.php"); ?>
+        </div>
+    </div>
+</body>
+
+</html>
