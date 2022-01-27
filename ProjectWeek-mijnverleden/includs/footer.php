c9199afc6b563cb73c80d34fdfa9cdcf347cf502
Tim-Schaefer <75988546+Tim-Schaefer@users.noreply.github.com>
2022-01-27 14:33:40 +0100

    added photos

---
+++ ProjectWeek-mijnverleden/includs/footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="footer-txt-menu-container">
+    <div class="footer-text-container">
+
+        <p class="footer-txt">Welkom op onze website hier informeren we je graag meer
+            over Limburg-Mijnverleden,
+
+            Om eerlijk te zijn, is het wel saai om alleen maar te lezen en te leren,
+            Dus hier geven we je video's om te zien en Quiz om te maken...
+            Veel plezier en leer meer</p>
+    </div>
+    <nav class="footer-nav">
+        <ul class="footer-menu">
+           <li><a href="#"  class="footer-item">Team </a></li>
+           <li><a href="#" class="footer-item">Nieuws about the site</a></li>
+
+           <li><a href="#" class="footer-item">Vidoes</a></li>
+     
+
+        </ul>
+    </nav>
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ ProjectWeek-mijnverleden/includs/menu.php
@@ -0,0 +1,19 @@
+ <!-- banner voor de site -->
+ <div class="banner_container">
+
+<img class="banner" src="./img/banner-limburgmijnverleden.jpg" alt="Foto niet gevonden">
+
+</div>
+<!-- End banner voor de site -->
+<header>
+
+    <nav>
+        <ul class="top_menu">
+            <li><a  class="Item"href="#"><i class="fas fa-home"></i>Home</a></li>
+            <li><a class="Item" href="#"><i class="fas fa-info-circle"></i>About</a></li>
+            <li><a class="Item" href="#"><i class="fas fa-book"></i>Opdrachten</a></li>
+            <li><a class="Item" href="#"><i class="fas fa-mobile-alt"></i>Contact</a></li>
+        </ul>
+    </nav>
+     
+</header>
